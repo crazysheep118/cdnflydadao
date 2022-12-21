ba455da5a6a60a92b71fa09fcbdec4ca333c1199
crazysheep118 <61119233+crazysheep118@users.noreply.github.com>
2022-12-21 09:25:40 +0800

    Add files via upload

---
+++ backup/restore_master.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -x
+
+#判断系统版本
+check_sys(){
+    local checkType=$1
+    local value=$2
+
+    local release=''
+    local systemPackage=''
+    local packageSupport=''
+
+    if [[ "$release" == "" ]] || [[ "$systemPackage" == "" ]] || [[ "$packageSupport" == "" ]];then
+
+        if [[ -f /etc/redhat-release ]];then
+            release="centos"
+            systemPackage="yum"
+            packageSupport=true
+
+        elif cat /etc/issue | grep -q -E -i "debian";then
+            release="debian"
+            systemPackage="apt"
+            packageSupport=true
+
+        elif cat /etc/issue | grep -q -E -i "ubuntu";then
+            release="ubuntu"
+            systemPackage="apt"
+            packageSupport=true
+
+        elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat";then
+            release="centos"
+            systemPackage="yum"
+            packageSupport=true
+
+        elif cat /proc/version | grep -q -E -i "debian";then
+            release="debian"
+            systemPackage="apt"
+            packageSupport=true
+
+        elif cat /proc/version | grep -q -E -i "ubuntu";then
+            release="ubuntu"
+            systemPackage="apt"
+            packageSupport=true
+
+        elif cat /proc/version | grep -q -E -i "centos|red hat|redhat";then
+            release="centos"
+            systemPackage="yum"
+            packageSupport=true
+
+        else
+            release="other"
+            systemPackage="other"
+            packageSupport=false
+        fi
+    fi
+
+    echo -e "release=$release\nsystemPackage=$systemPackage\npackageSupport=$packageSupport\n" > /tmp/ezhttp_sys_check_result
+
+    if [[ $checkType == "sysRelease" ]]; then
+        if [ "$value" == "$release" ];then
+            return 0
+        else
+            return 1
+        fi
+
+    elif [[ $checkType == "packageManager" ]]; then
+        if [ "$value" == "$systemPackage" ];then
+            return 0
+        else
+            return 1
+        fi
+
+    elif [[ $checkType == "packageSupport" ]]; then
+        if $packageSupport;then
+            return 0
+        else
+            return 1
+        fi
+    fi
+}
+
+
+# 停止主控程序
+supervisorctl -c /opt/cdnfly/master/conf/supervisord.conf stop all
+
+# 恢复mysql
+eval `grep "MYSQL_PASS" /opt/cdnfly/master/conf/config.py`
+mysql -uroot -p$MYSQL_PASS "drop database cdn;create database cdn;"
+gunzip < /root/cdn.sql.gz |  mysql -uroot -p$MYSQL_PASS cdn 
+
+# 启动主控程序
+supervisorctl -c /opt/cdnfly/master/conf/supervisord.conf start all
+
+
